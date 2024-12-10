2ed54bad3e014f7f7c1bf93522ed91fd23dba761
faizaledrus <faizal.edrus@gmail.com>
2024-12-11 00:29:26 +0700

    running

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <!-- Display user data -->
+    <div v-if="userData">
+      <h2>User Details</h2>
+      <pre>{{ userData }}</pre>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,10 +19,17 @@ export default {
   components: {
     HelloWorld
   },
-  async mounted(){
-    
-    const data = await window.ghl.getUserData();
-    console.log("user-details", data)
+  data() {
+    return {
+      userData: null
+    }
+  },
+  async mounted() {
+    try {
+      this.userData = await window.ghl.getUserData();
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
   }
 }
 </script>
