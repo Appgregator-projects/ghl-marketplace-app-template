608e076369e11fa423333800193474807c6d4dbb
faizaledrus <faizal.edrus@gmail.com>
2024-12-11 00:37:45 +0700

    tambah yahemha

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="yahemhahhhh to Your Vue.js App"/>
     
     <!-- Display user data -->
     <div v-if="userData">
